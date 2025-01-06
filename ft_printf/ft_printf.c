3f2342e356bd1fdace90a8df8f14720e2f461c8a
lehMaxi <lehmann.max95@gmail.com>
2025-01-06 13:12:44 +0100

    print update

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,8 +6,49 @@
 /*   By: mlehmann <mlehmann@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 12:43:33 by mlehmann          #+#    #+#             */
-/*   Updated: 2024/12/11 12:43:47 by mlehmann         ###   ########.fr       */
+/*   Updated: 2025/01/06 12:56:05 by mlehmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
+int	call_sort(char %, va_list *arg)
+{
+	if (% == 'c')
+		ft_putchar(va_arg(arg, char));
+	else if (% == 's')
+		ft_putstring(va_arg(arg, char*));
+	else if (% == 'p')
+		ft_putpointer(va_arg(arg, void*));
+	else if (% == 'd' || % == 'i')
+		ft_putdecimal(va_arg(arg, int), 0);
+	else if (% == 'u')
+		ft_putdecimal(0, va_arg(arg, unsigned int));
+	else if (% == 'x')
+		ft_puthex(va_arg(arg, int), 'x');
+	else if (% == 'X')
+		ft_puthex(va_arg(arg, int), 'X');
+	else if (% == '%')
+		ft_putchar('%');
+}
+
+int	ft_printf(const char *str, ...)
+{
+	int	i;
+	va_list	ap;
+
+	i = 0;
+	va_start(ap, str);
+	while (str[i])
+	{
+		if (str[i] != '%')
+		{
+			ft_putchar(str[i]);
+			i++;
+		}
+		else
+		{
+			call_sort(str[i+1], *ap);
+		}
+	}
+}
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,8 +6,21 @@
 /*   By: mlehmann <mlehmann@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 12:43:13 by mlehmann          #+#    #+#             */
-/*   Updated: 2024/12/11 12:43:16 by mlehmann         ###   ########.fr       */
+/*   Updated: 2025/01/06 10:42:07 by mlehmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
 
+int	ft_printf(const char *s, ...);//writes input and calls the rest
+int	ft_putchar(char c);//writes a char
+int	ft_putstring(char *s);// writes a string
+int	ft_putdecimal(int n, unsigned int u);// writes an int, signed or not
+int	ft_putpointer(void *p);// writes the adress of a pointer in hexadecimal
+int	ft_puthex(int n, char c);// puts a hexadecimal on the terminal
+int	ft_putperc(char c);// writes a % (cause it is scarry it needs its own)
+#endif
